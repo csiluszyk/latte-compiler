dd2d24b8d1dbb1dedd78d10d1786655962842ad3
Cezary Siłuszyk <cs321191@students.mimuw.edu.pl>
2017-12-30 14:23:39 +0100

    Add grammar

---
+++ src/latc.hs
@@ -0,0 +1,43 @@
+import System.Environment (getArgs, getProgName)
+import System.IO (
+  withFile, hGetContents, Handle, IOMode (ReadMode), stdin, stderr, hPutStrLn
+  )
+import System.Exit (exitFailure, exitSuccess)
+
+import ErrM
+import LexLatte
+import ParLatte
+import AbsLatte
+
+import qualified Frontend
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> compile stdin  -- TODO
+    [filename] -> withFile filename ReadMode compile
+    _ -> usage
+
+usage :: IO ()
+usage = getProgName >>= (\s -> putStrLn $ "Usage: " ++ s ++ " FILENAME")
+
+compile :: Handle -> IO ()
+compile hFile = do
+  fileContent <- hGetContents hFile
+  case pProgram (tokens fileContent) of
+    Bad s -> do
+      hPutStrLn stderr "BAD"
+      hPutStrLn stderr "Parse failed..."
+      hPutStrLn stderr s
+      exitFailure
+    Ok prog ->
+      case Frontend.eval prog of
+        Left err -> do
+          hPutStrLn stderr "BAD"
+          hPutStrLn stderr err
+          exitFailure
+        Right symTab -> do
+          -- result :: [Value]
+          print symTab
+          exitSuccess
